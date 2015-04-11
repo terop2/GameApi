b11ad5dfe656e4c9f3a1105483910c8a9afff95b
Tero Pulkkinen <terop@kotiposti.net>
2015-04-11 06:15:33 +0300

    new features to boulders.

--- GameApi/02-moving-boulders/test.cpp
+++ GameApi/02-moving-boulders/test.cpp
@@ -3,19 +3,41 @@
 
 using namespace GameApi;
 
+
 char world[] = 
-  "O........"
-  "XOOOOOO.."
-  "OXXXXXOO."
-  "......XX."
-  "...@....."
-  ".........";
+  "+++++++++"
+  "+OOOOOO.+"
+  "+XXXXXOO+"
+  "+.....XX+"
+  "+..@....+"
+  "+++++++++";
+
+const int number_of_pieces = 5;
+
+
+bool blocking_piece(int c)
+{
+  switch(c) {
+  case 0: return true;
+  case 1:
+  case 2:
+  case 3:
+    return false;
+  case 4: return true;
+  };
+}
 
 P pieces(int c, EveryApi &ev)
 {
-  //std::cout << c << std::endl;
+  // std::cout << c << std::endl;
   switch(c)
     {
+    case 4:
+      {
+      P p = ev.polygon_api.cube(0.0, 100.0, 0.0, 100.0, 0.0, 100.0);
+      P p2 = ev.polygon_api.color_faces(p, 0xff884422, 0xff442211, 0xff884422, 0xff442211);
+      return p2;
+      }
     case 0:
       {
       PT center = ev.point_api.point(50.0,50.0,50.0);
@@ -57,6 +79,7 @@ int pieces_2(char c)
   case 'X': return 1;
   case '.': return 2;
   case '@': return 3;
+  case '+': return 4;
   }
   return 1;
 }
@@ -81,11 +104,17 @@ int main() {
   //P p2 = ev.polygon_api.rotatex(p3, 90.0*3.14159*2.0/360.0);
 
 
-  WorldObj poly(ev, std::bind(&pieces, _1, std::ref(ev)), 4, bm, 100.0, 100.0, sh);
+  WorldObj poly(ev, std::bind(&pieces, _1, std::ref(ev)), number_of_pieces, bm, 100.0, 100.0, sh);
   poly.set_pos(-500.0, 400.0, 0.0);
   poly.set_rotation_matrix(ev.matrix_api.xrot(90.0*3.14159*2.0/360.0));
   poly.prepare();
  
+  int player_pos_x = 3;
+  int player_pos_y = 4;
+
+  int old_player_pos_x;
+  int old_player_pos_y;
+
   while(1) {
     // clear frame buffer
     ev.mainloop_api.clear_3d();
@@ -99,7 +128,21 @@ int main() {
     // handle esc event
     MainLoopApi::Event e = ev.mainloop_api.get_event();
     if (e.ch==27) break;
-    if (e.ch=='a') { poly.set_block(3,3,0); }
+    poly.set_block(player_pos_x,player_pos_y,2);
+    old_player_pos_x = player_pos_x;
+    old_player_pos_y = player_pos_y;
+    if (e.ch=='a'&&e.type==0x300) { player_pos_y--; }
+    if (e.ch=='z'&&e.type==0x300) { player_pos_y++; }
+    if (e.ch==','&&e.type==0x300) { player_pos_x--; }
+    if (e.ch=='.'&&e.type==0x300) { player_pos_x++; }
+    int val = poly.read_block(player_pos_x, player_pos_y);
+    if (blocking_piece(val))
+      {
+	player_pos_x = old_player_pos_x;
+	player_pos_y = old_player_pos_y;
+      }
+
+    poly.set_block(player_pos_x, player_pos_y,3);
   }
 
 
