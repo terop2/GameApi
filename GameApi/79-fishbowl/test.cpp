cc57afdabe984c645490bbe260e213a3b46e4321
Tero Pulkkinen <terop@kotiposti.net>
2015-06-06 00:23:15 +0300

    bg.

--- GameApi/79-fishbowl/test.cpp
+++ GameApi/79-fishbowl/test.cpp
@@ -31,9 +31,28 @@ int main() {
   // shader initialization
   ev.shader_api.load("Shader.txt");
   SH sh = ev.shader_api.colour_shader();
-
+  SH sh2 = ev.shader_api.texture_shader();
   // rest of the initializations
   ev.mainloop_api.init_3d(sh);
+  ev.mainloop_api.init_3d(sh2);
+
+  PT pp1 = ev.point_api.point(-1900.0, -1900.0, -800.0);
+  PT pp2 = ev.point_api.point(1900.0, -1900.0, -800.0);
+  PT pp3 = ev.point_api.point(1900.0, 1900.0, -800.0);
+  PT pp4 = ev.point_api.point(-1900.0, 1900.0, -800.0);
+  P bg = ev.polygon_api.quad(pp1,pp2,pp3,pp4);
+  BM sea = ev.bitmap_api.loadbitmap("water_blue_sea.jpg");
+  TX tex0 = ev.texture_api.tex_plane(1900*2,1900*2);
+  TX tex = ev.texture_api.tex_assign(tex0, 0, 0,0, sea);
+  TXID tex2 = ev.texture_api.prepare(tex);
+  Q q_bm = ev.texture_api.get_tex_coord(tex, 0);
+  P bg2 = ev.polygon_api.sprite_bind(bg, q_bm, tex);
+  P bg3 = ev.polygon_api.color_faces(bg2, 0x88888888, 0x88888888, 0x88888888, 0x88888888);
+  //P bg4 = ev.polygon_api.texture(bg3, sea);
+  PolygonObj bg_obj(ev, bg3, sh2);
+  bg_obj.bind_texture(0, tex2);
+  bg_obj.prepare();
+
 
   PT pos1 = ev.point_api.point(0.0, 0.0, 0.0);
   P p1 = ev.polygon_api.sphere(pos1, 400.0,20,20);
@@ -63,6 +82,9 @@ int main() {
     poly.render();
     lines_obj.set_rotation_matrix(ev.matrix_api.yrot(frame*3.14159*2.0/360.0));
     lines_obj.render();
+    ev.texture_api.use(tex2);
+    bg_obj.render();
+    ev.texture_api.unuse(tex2);
     ev.mainloop_api.fpscounter();
     // swapbuffers
     ev.mainloop_api.swapbuffers();
