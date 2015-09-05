2695d82887535b50ef75b8dc0b119ebcac0851d0
Tero Pulkkinen <terop@kotiposti.net>
2015-09-05 22:02:42 +0300

    new examples ported to emscripten.

---
+++ GameApi/example3d_2/test.cpp
@@ -0,0 +1,66 @@
+#include "GameApi.hh"
+
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
+
+using namespace GameApi;
+
+struct Envi {
+  EveryApi *ev;
+  PolygonObj *poly;
+} env;
+
+void iter()
+{
+    env.ev->mainloop_api.clear_3d();
+
+    env.poly->render();
+
+    env.ev->mainloop_api.fpscounter();
+    // swapbuffers
+    env.ev->mainloop_api.swapbuffers();
+
+    // handle esc event
+    MainLoopApi::Event e = env.ev->mainloop_api.get_event();
+#ifndef EMSCRIPTEN
+    if (e.ch==27) { exit(0); }
+#endif
+}
+
+int main() {
+  Env e;
+  EveryApi ev(e);
+
+  // initialize window
+  ev.mainloop_api.init_window();
+
+  // shader initialization
+  ev.shader_api.load_default();
+  SH sh = ev.shader_api.colour_shader();
+
+  // rest of the initializations
+  ev.mainloop_api.init_3d(sh);
+
+
+
+  P p3 = ev.polygon_api.cube(0.0, 100.0, 0.0, 100.0, 0.0, 100.0);
+  PolygonObj poly(ev, p3, sh);
+  poly.prepare();
+
+  env.ev = &ev;
+  env.poly = &poly;
+
+#ifndef EMSCRIPTEN
+  while(1) {
+    iter();
+    ev.mainloop_api.delay(10);
+  }
+#else
+  emscripten_set_main_loop(iter, 60,1);
+#endif
+
+
+
+}
