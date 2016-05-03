bc801f5d12d7088e31bb32f9067526c946cb3202
Tero Pulkkinen <terop@kotiposti.net>
2016-05-03 06:42:37 +0300

    fixed color() function in PolygonApi. It now forwards calls properly.

--- GameApi/GameApi_pl.cc
+++ GameApi/GameApi_pl.cc
@@ -922,14 +922,19 @@ EXPORT GameApi::P GameApi::PolygonApi::color_lambert(P orig, unsigned int color,
   return add_polygon(e, coll,1);
   
 }
+class ColorElem2 : public ForwardFaceCollection
+{
+public:
+  ColorElem2(FaceCollection *coll, unsigned int col) : ForwardFaceCollection(*coll), col(col) { }
+  unsigned int Color(int face, int point) const {return col; }
+private:
+  unsigned int col;
+};
+
 EXPORT GameApi::P GameApi::PolygonApi::color(P next, unsigned int color)
 {
-  ::EnvImpl *env = ::EnvImpl::Environment(&e);
   FaceCollection *c = find_facecoll(e, next);
-  BoxableFaceCollectionConvert *convert = new BoxableFaceCollectionConvert(*c);
-  env->deletes.push_back(std::shared_ptr<void>(convert));  
-  if (!c) { std::cout << "dynamic cast failed" << std::endl; }
-  FaceCollection *coll = new ColorElem(*convert, color);
+  FaceCollection *coll = new ColorElem2(c, color);
   return add_polygon(e, coll,1);
 }
 class ColorGrayScale : public ForwardFaceCollection
@@ -2682,7 +2687,7 @@ EXPORT void GameApi::PolygonApi::render_vertex_array(VA va)
 }
 EXPORT void GameApi::PolygonApi::prepare_vertex_array_instanced(ShaderApi &shapi, VA va, PTA pta, SH sh)
 {
-  VertexArraySet *s = find_vertex_array(e, va);
+  //VertexArraySet *s = find_vertex_array(e, va);
   RenderVertexArray *rend = find_vertex_array_render(e, va);
   PointArray3 *arr = find_point_array3(e, pta);
   rend->prepare_instanced(0, (Point*)arr->array, arr->numpoints);
