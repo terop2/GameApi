9e40e0733e4ba8a1709e5c99f8cd05ba86f3125b
Tero Pulkkinen <terop@kotiposti.net>
2016-07-18 00:17:59 +0300

    fix compile-error.

--- GameApi/GameApi_h.hh
+++ GameApi/GameApi_h.hh
@@ -525,7 +525,6 @@ struct EnvImpl
   std::vector<Curve<Point>*> curves;
   std::vector<MatrixArray*> matrix_arrays;
   std::vector<Curve<Matrix>*> matrix_curves;
-  std::vector<AccelStructure*> accel_structure;
   //std::vector<EventInfo> event_infos;
   Sequencer2 *event_infos; // owned, one level only.
   pthread_mutex_t mutex;
@@ -796,7 +795,6 @@ struct FaceCollPolyHandle : public PolyHandle
 //
 // add functions
 //
-GameApi::AS add_accel_structure(GameApi::Env &e, AccelStructure *accel);
 GameApi::MC add_matrix_curve(GameApi::Env &e, Curve<Matrix> *curve);
 GameApi::MS add_matrix_array(GameApi::Env &e, MatrixArray *arr);
 GameApi::C add_curve(GameApi::Env &e, Curve<Point> *curve);
@@ -880,7 +878,6 @@ GameApi::CT add_cutter(GameApi::Env &e, Cutter *cut);
 //
 // find() functions
 //
-AccelStructure *find_accel_structure(GameApi::Env &e, GameApi::AS a);
 Curve<Matrix> *find_matrix_curve(GameApi::Env &e, GameApi::MC m);
 MatrixArray *find_matrix_array(GameApi::Env &e, GameApi::MS m);
 Curve<Point> *find_curve(GameApi::Env &e, GameApi::C c);
