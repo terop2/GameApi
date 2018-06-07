68f3ac67a02c46915ee7d8355e5c80972a4f8ca6
Tero Pulkkinen <terop@kotiposti.net>
2018-06-07 03:29:33 +0300

    low.

--- GameApi/GameApi_low.cc
+++ GameApi/GameApi_low.cc
@@ -35,6 +35,11 @@
 #endif
 
 
+#ifdef WINDOWS
+#define GLEW_HACK
+#endif
+
+#undef glBindAttribLocation
 #undef glActiveTexture
 #undef glClientActiveTexture
 #undef glTexStorage3d
@@ -130,24 +135,28 @@ public:
   virtual void glTexParameteri(int ID, int a,int b) { ::glTexParameteri(ID,a,b); }
   virtual void glHint(int hint, int value) { ::glHint(hint,value); }
   virtual void glActiveTexture(int a) { 
+#ifdef GLEW_HACK
 #define glActiveTexture GLEW_GET_FUN(__glewActiveTexture)
-    glActiveTexture(a); 
-#undef glActiveTexture
+#endif
+    ::glActiveTexture(a); 
   }
   virtual void glClientActiveTexture(int a) { 
+#ifdef GLEW_HACK
 #define glClientActiveTexture GLEW_GET_FUN(__glewClientActiveTexture)
-    glClientActiveTexture(a); 
-#undef glClientActiveTexture
+#endif
+::glClientActiveTexture(a); 
   }
   virtual void glTexStorage3D(int arr, int a, int flag, int w, int h, int layer_count) { 
+#ifdef GLEW_HACK
 #define glTexStorage3d GLEW_GET_FUN(__glewTexStorage3D)
-    glTexStorage3d(arr,a,flag,w,h,layer_count); 
-#undef glTexStorage3d
+#endif
+::glTexStorage3d(arr,a,flag,w,h,layer_count); 
 }
   virtual void glTexSubImage3D(int arr, int a,int b,int c,int d,int e,int f, int g, int rgba, int unsig_byte, void *buffer) { 
+#ifdef GLEW_HACK
 #define glTexSubImage3d GLEW_GET_FUN(__glewTexSubImage3D)
-    glTexSubImage3d(arr,a,b,c,d,e,f,g,rgba,unsig_byte,buffer); 
-#undef glTexSubImage3d
+#endif
+::glTexSubImage3d(arr,a,b,c,d,e,f,g,rgba,unsig_byte,buffer); 
   }
   virtual void glGetTexLevelParameteriv(int a, int b, int w, int *ptr) { glGetTexLevelParameteriv(a,b,w,ptr); }
   virtual void glGetTexImage(int a, int b, int rgba, int unsign_byte, void *ptr) { ::glGetTexImage(a,b,rgba,unsign_byte,ptr); }
@@ -162,73 +171,88 @@ public:
 
   // vertex arrays
   virtual void glGenBuffers(int i, unsigned int *buffers) { 
+#ifdef GLEW_HACK
 #define glGenBuffers GLEW_GET_FUN(__glewGenBuffers)
-    glGenBuffers(i,buffers); 
-#undef glGenBuffers
+#endif
+::glGenBuffers(i,buffers); 
   }
   virtual void glDeleteBuffers(int count, const unsigned int *buffers) { 
+#ifdef GLEW_HACK
 #define glDeleteBuffers GLEW_GET_FUN(__glewDeleteBuffers)
-    glDeleteBuffers(count,buffers); 
-#undef glDeleteBuffers
+#endif
+::glDeleteBuffers(count,buffers); 
   }
   virtual void glBindBuffer(int a, unsigned int data) { 
+#ifdef GLEW_HACK
 #define glBindBuffer GLEW_GET_FUN(__glewBindBuffer)
-    glBindBuffer(a,data); 
-#undef glBindBuffer
+#endif
+::glBindBuffer(a,data); 
   }
   virtual void glBufferData(int a, int size, const void *ptr, int static_draw) { 
+#ifdef GLEW_HACK
 #define glBufferData GLEW_GET_FUN(__glewBufferData)
-    glBufferData(a,size,ptr,static_draw); 
-#undef glBufferData
+#endif
+    ::glBufferData(a,size,ptr,static_draw); 
   }
   virtual void glBufferSubData(int a, int b, int c, const void* d) { 
+#ifdef GLEW_HACK
 #define glBufferSubData GLEW_GET_FUN(__glewBufferSubData)
-    glBufferSubData(a,b,c,d);
-#undef glBufferSubData
+#endif
+::glBufferSubData(a,b,c,d);
   }
   virtual void glVertexAttribPointer(int a, int b, int gl_float, int boolean, int c, const void *ptr) { 
+#ifdef GLEW_HACK
 #define glVertexAttribPointer GLEW_GET_FUN(__glewVertexAttribPointer)
-    glVertexAttribPointer(a,b,gl_float,boolean,c,ptr); 
-#undef glVertexAttribPointer
+#endif
+::glVertexAttribPointer(a,b,gl_float,boolean,c,ptr); 
   }
 virtual void glVertexAttribIPointer(int a, int b, int gl_float, int boolean, const void *ptr) {
+#ifdef GLEW_HACK
 #define glVertexAttribIPointer GLEW_GET_FUN(__glewVertexAttribIPointer)
-    glVertexAttribIPointer(a,b,gl_float,boolean,ptr); 
-#undef glVertexAttribIPointer
+#endif
+::glVertexAttribIPointer(a,b,gl_float,boolean,ptr); 
 }
   virtual void glVertexAttribDivisor(int a, int b) { 
+#ifdef GLEW_HACK
 #define glVertexAttribDivisor GLEW_GET_FUN(__glewVertexAttribDivisor)
-    glVertexAttribDivisor(a,b); 
-#undef glVertexAttribDivisor
+#endif
+    ::glVertexAttribDivisor(a,b); 
   }
   virtual void glGenVertexArrays(int i, unsigned int *arr) { 
+#ifdef GLEW_HACK
 #define glGenVertexArrays GLEW_GET_FUN(__glewGenVertexArrays)
-    glGenVertexArrays(i,arr); 
-#undef glGenVertexArrays
+#endif
+    ::glGenVertexArrays(i,arr); 
   }
   virtual void glDeleteVertexArrays(int count, unsigned int *vao) { 
+#ifdef GLEW_HACK
 #define glDeleteVertexArrays GLEW_GET_FUN(__glewDeleteVertexArrays)
-    glDeleteVertexArrays(count,vao); 
-#undef glDeleteVertexArrays
+#endif
+::glDeleteVertexArrays(count,vao); 
   }
   virtual void glBindVertexArray(int vao) { 
+#ifdef GLEW_HACK
 #define glBindVertexArray GLEW_GET_FUN(__glewBindVertexArray)
-    glBindVertexArray(vao); 
-#undef glBindVertexArray
+#endif
+::glBindVertexArray(vao); 
   }
   virtual void glEnableVertexAttribArray(int a) { 
+#ifdef GLEW_HACK
 #define glEnableVertexAttribArray GLEW_GET_FUN(__glewEnableVertexAttribArray)
-    glEnableVertexAttribArray(a); 
-#undef glEnableVertexAttribArray
+#endif
+::glEnableVertexAttribArray(a); 
   }
   virtual void glDisableVertexAttribArray(int a) { 
+#ifdef GLEW_HACK
 #define glDisableVertexAttribArray GLEW_GET_FUN(__glewDisableVertexAttribArray)
-    glDisableVertexAttribArray(a); 
-#undef glDisableVertexAttribArray
+#endif
+::glDisableVertexAttribArray(a); 
   }
   virtual void glDrawArraysInstanced(int tri, int a, int b, int c) { 
+#ifdef GLEW_HACK
 #define glDrawArraysInstanced GLEW_GET_FUN(__glewDrawArraysInstanced)
-    glDrawArraysInstanced(tri,a,b,c); 
+#endif
+::glDrawArraysInstanced(tri,a,b,c); 
   }
   virtual void glDrawArrays(int tri, int a, int b) { ::glDrawArrays(tri,a,b); }
   
@@ -237,106 +261,165 @@ virtual void glVertexAttribIPointer(int a, int b, int gl_float, int boolean, con
   
   // fbo
   virtual void glGenFramebuffers(int i, unsigned int *fbo_id) {
+#ifdef GLEW_HACK
 #define glGenFramebuffers GLEW_GET_FUN(__glewGenFramebuffers) 
-    glGenFramebuffers(i,fbo_id); }
+#endif
+::glGenFramebuffers(i,fbo_id); }
   virtual void glGenRenderBuffers(int i, unsigned int *rbo_id) { ::glGenRenderbuffers(i,rbo_id); }
   virtual void glDeleteRenderBuffers(int i, unsigned int *rbo) { ::glDeleteRenderbuffers(i,rbo); }
   virtual void glDeleteFrameBuffers(int i, unsigned int *fbo) { ::glDeleteFramebuffers(i,fbo); }
   virtual void glBindFramebuffer(int a, int fbo_id) { 
+#ifdef GLEW_HACK
 #define glBindFramebuffer GLEW_GET_FUN(__glewBindFramebuffer)
-    glBindFramebuffer(a,fbo_id); }
+#endif
+::glBindFramebuffer(a,fbo_id); }
   virtual void glBindRenderbuffer(int a, int fbo_id) { 
+#ifdef GLEW_HACK
 #define glBindRenderbuffer GLEW_GET_FUN(__glewBindRenderbuffer)
-glBindRenderbuffer(a,fbo_id); }
+#endif
+::glBindRenderbuffer(a,fbo_id); }
   virtual void glRenderbufferStorage(int a, int d, int sx, int sy) { 
+#ifdef GLEW_HACK
 #define glRenderbufferStorage GLEW_GET_FUN(__glewRenderbufferStorage)
-glRenderbufferStorage(a,d,sx,sy); }
+#endif
+::glRenderbufferStorage(a,d,sx,sy); }
   virtual void glFramebufferRenderbuffer(int a, int d, int r, unsigned int tex) { 
+#ifdef GLEW_HACK
 #define glFramebufferRenderbuffer GLEW_GET_FUN(__glewFramebufferRenderbuffer)
-glFramebufferRenderbuffer(a,d,r,tex); }
+#endif
+::glFramebufferRenderbuffer(a,d,r,tex); }
   
   // shaders
   virtual void glCreateShader(int shader) { 
+#ifdef GLEW_HACK
 #define glCreateShader GLEW_GET_FUN(__glewCreateShader)
-glCreateShader(shader); }
+#endif
+::glCreateShader(shader); }
   virtual void glShaderSource(int h, int c, const char **strings, int *lengths) { 
+#ifdef GLEW_HACK
 #define glShaderSource GLEW_GET_FUN(__glewShaderSource)
+#endif
 ::glShaderSource(h,c,strings,lengths); }
   virtual void glCompileShader(int h) { 
+#ifdef GLEW_HACK
 #define glCompileShader GLEW_GET_FUN(__glewCompileShader)
-glCompileShader(h); }
+#endif
+::glCompileShader(h); }
   virtual void glGetShaderInfoLog(unsigned int h, int val, int *length, char *buf) { 
+#ifdef GLEW_HACK
 #define glGetShaderInfoLog GLEW_GET_FUN(__glewGetShaderInfoLog)
-glGetShaderInfoLog(h,val,length,buf); }
+#endif
+::glGetShaderInfoLog(h,val,length,buf); }
   virtual void glGetShaderiv(int handle, int gl_compile_status, int *ptr) { 
+#ifdef GLEW_HACK
 #define glGetShaderiv GLEW_GET_FUN(__glewGetShaderiv)
-glGetShaderiv(handle,gl_compile_status, ptr); }
+#endif
+::glGetShaderiv(handle,gl_compile_status, ptr); }
   virtual void glDeleteShader(int h) { 
+#ifdef GLEW_HACK
 #define glDeleteShader GLEW_GET_FUN(__glewDeleteShader)
-glDeleteShader(h); }
+#endif
+::glDeleteShader(h); }
   
   // programs
   virtual int glCreateProgram() { return 
+#ifdef GLEW_HACK
 #define glCreateProgram GLEW_GET_FUN(__glewCreateProgram)
-glCreateProgram(); }
+#endif
+      ::glCreateProgram(); }
   virtual void glDeleteProgram(int p) { 
+#ifdef GLEW_HACK
 #define glDeleteProgram GLEW_GET_FUN(__glewDeleteProgram)
-glDeleteProgram(p); }
+#endif
+::glDeleteProgram(p); }
   virtual  void glAttachShader(int p, int h) { 
+#ifdef GLEW_HACK
 #define glAttachShader GLEW_GET_FUN(__glewAttachShader)
-glAttachShader(p,h); }
+#endif
+::glAttachShader(p,h); }
   virtual void glDetachShader(int p, int h) { 
+#ifdef GLEW_HACK
 #define glDetachShader GLEW_GET_FUN(__glewDetachShader)
-glDetachShader(p,h); }
+#endif
+::glDetachShader(p,h); }
   virtual void glLinkProgram(int p) { 
+#ifdef GLEW_HACK
 #define glLinkProgram GLEW_GET_FUN(__glewLinkProgram)
-glLinkProgram(p); }
+#endif
+::glLinkProgram(p); }
   virtual void glUseProgram(int p) { 
+#ifdef GLEW_HACK
 #define glUseProgram GLEW_GET_FUN(__glewUseProgram)
-glUseProgram(p); }
+#endif
+    ::glUseProgram(p); }
   virtual void glGetProgramInfoLog(unsigned int p, int num, int *len, char *buf) { 
+#ifdef GLEW_HACK
 #define glGetProgramInfoLog GLEW_GET_FUN(__glewGetProgramInfoLog)
-glGetProgramInfoLog(p,num,len,buf); }
+#endif
+::glGetProgramInfoLog(p,num,len,buf); }
   virtual void glBindFragDataLocation(int p, int num, char *data) { 
+#ifdef GLEW_HACK
 #define glBindFragDataLocation GLEW_GET_FUN(__glewBindFragDataLocation)
-glBindFragDataLocation(p,num,data); }
-#undef glBindAttribLocation
+#endif
+::glBindFragDataLocation(p,num,data); }
   virtual void glBindAttribLocation(int p, int num, char *data) { 
+#ifdef GLEW_HACK
 #define glBindAttribLocation GLEW_GET_FUN(__glewBindAttribLocation)
-    glBindAttribLocation(p,num,data); 
+#endif
+::glBindAttribLocation(p,num,data); 
   }
   virtual void glProgramParameteriEXT(int p, int geom, int inputtype) { 
+#ifdef GLEW_HACK
 #define glProgramParameteriEXT GLEW_GET_FUN(__glewProgramParameteriEXT)
-glProgramParameteriEXT(p,geom,inputtype); }
+#endif
+::glProgramParameteriEXT(p,geom,inputtype); }
   
   // uniforms
   virtual int glGetUniformLocation(int p, char *data) { 
+#ifdef GLEW_HACK
 #define glGetUniformLocation GLEW_GET_FUN(__glewGetUniformLocation)
-return glGetUniformLocation(p,data); }
+#endif
+return ::glGetUniformLocation(p,data); }
   virtual void glUniform1f(int loc, float val) { 
+#ifdef GLEW_HACK
 #define glUniform1f GLEW_GET_FUN(__glewUniform1f)
-glUniform1f(loc,val); }
+#endif
+::glUniform1f(loc,val); }
   virtual void glUniform2f(int loc, float val, float val2) { 
+#ifdef GLEW_HACK
 #define glUniform2f GLEW_GET_FUN(__glewUniform2f)
-glUniform2f(loc,val,val2); }
+#endif
+::glUniform2f(loc,val,val2); }
   virtual void glUniform3f(int loc, float val, float val2, float val3) { 
+#ifdef GLEW_HACK
 #define glUniform3f GLEW_GET_FUN(__glewUniform3f)
-glUniform3f(loc, val, val2, val3); }
+#endif
+::glUniform3f(loc, val, val2, val3); }
   virtual void glUniform4f(int loc, float val, float val2, float val3, float val4) { 
+#ifdef GLEW_HACK
 #define glUniform4f GLEW_GET_FUN(__glewUniform4f)
-glUniform4f(loc,val,val2,val3, val4); }
+#endif
+::glUniform4f(loc,val,val2,val3, val4); }
   virtual void glUniform1iv(int loc, int count, int *array) { 
+#ifdef GLEW_HACK
 #define glUniform1iv GLEW_GET_FUN(__glewUniform1iv)
-glUniform1iv(loc,count,array); }
+#endif
+::glUniform1iv(loc,count,array); }
   virtual void glUniform1fv(int loc, int count, float *array) { 
+#ifdef GLEW_HACK
 #define glUniform1fv GLEW_GET_FUN(__glewUniform1fv)
-glUniform1fv(loc,count,array); }
+#endif
+::glUniform1fv(loc,count,array); }
   virtual void glUniformMatrix4fv(int loc, int count, int boolean, float *matrix) { 
+#ifdef GLEW_HACK
 #define glUniformMatrix4fv GLEW_GET_FUN(__glewUniformMatrix4fv)
-glUniformMatrix4fv(loc,count,boolean,matrix); }
+#endif
+::glUniformMatrix4fv(loc,count,boolean,matrix); }
   virtual void glUniform3fv(int loc, int count, float *arr) { 
+#ifdef GLEW_HACK
 #define glUniform3fv GLEW_GET_FUN(__glewUniform3fv)
-glUniform3fv(loc,count,arr); }
+#endif
+::glUniform3fv(loc,count,arr); }
   
 
 
