17b48de3db4195e92d89e590f8f851e3c9ff9440
Tero Pulkkinen <terop@kotiposti.net>
2019-04-23 20:00:53 +0300

    ogg playing enabled.

--- GameApi/GameApi_main.cc
+++ GameApi/GameApi_main.cc
@@ -1644,11 +1644,11 @@ public:
       int s = ptr->size();
       for(int i=0;i<s;i++) ss.put(ptr->operator[](i));
       ss.close();
-#ifndef EMSCRIPTEN
+      //#ifndef EMSCRIPTEN
       ev.tracker_api.play_ogg("song.ogg");
-#else
-      std::cout << "Warning: ogg playing disabled since it didn't work in emscripten." << std::endl;
-#endif
+      //#else
+	// std::cout << "Warning: ogg playing disabled since it didn't work in emscripten." << std::endl;
+      //#endif
       firsttime = false;
     }
     next->execute(e);
