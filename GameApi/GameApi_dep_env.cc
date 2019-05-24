f8811c6f1a452ca2d3dd33647f7773584358fca5
Tero Pulkkinen <terop@kotiposti.net>
2019-05-24 07:51:21 +0300

    reverted.

--- GameApi/GameApi_dep_env.cc
+++ GameApi/GameApi_dep_env.cc
@@ -4,7 +4,6 @@
 
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
-#include <emscripten/fetch.h>
 #endif
 
 
@@ -320,18 +319,10 @@ ASyncCallback *rem_async_cb(std::string url)
 
 std::string striphomepage(std::string);
 void onprogress_async_cb(unsigned int tmp, void *, int, int) { }
-#ifndef EMSCRIPTEN
 void onerror_async_cb(unsigned int tmp, void *arg, int, const char*)
-#else
-void onerror_async_cb(emscripten_fetch_t *fetch)
-#endif
 {
   std::cout << "ERROR: url loading error! " << std::endl;
-#ifndef EMSCRIPTEN
   char *url = (char*)arg;
-#else
-  const char *url = fetch->url;
-#endif
     std::string url_str(url);
   std::string url_only(striphomepage(url_str));
     load_url_buffers_async[url_only] = (std::vector<unsigned char>*)-1;
@@ -351,20 +342,10 @@ std::string stripprefix(std::string s)
   int len = strlen("load_url.php?url=");
   return s.substr(len,s.size()-len);
 }
-#ifndef EMSCRIPTEN
 void onload_async_cb(unsigned int tmp, void *arg, void *data, unsigned int datasize)
-#else
-void onload_async_cb(emscripten_fetch_t *fetch)
-#endif
 {
-#ifndef EMSCRIPTEN
   char *url = (char*)arg;
   unsigned char *dataptr = (unsigned char*)data;
-#else
-  const char *url = fetch->url;
-  unsigned char *dataptr = (unsigned char*)fetch->data;
-  unsigned int datasize = fetch->numBytes;
-#endif
   if (datasize==0) {
       std::cout << "Empty URL file. Either url is broken or homepage is wrong." << std::endl;
   }
@@ -483,14 +464,17 @@ void ASyncLoader::load_urls(std::string url, std::string homepage)
     //    std::cout << "ASync pending inc (load_urls) -->" << async_pending_count << std::endl;
 
     //emscripten_async_wget_data(buf2, (void*)buf2 , &onload_async_cb, &onerror_async_cb);
-    //emscripten_async_wget2_data(buf2, "POST", url3.c_str(), (void*)buf3, 1, &onload_async_cb, &onerror_async_cb, &onprogress_async_cb);
+    emscripten_async_wget2_data(buf2, "POST", url3.c_str(), (void*)buf3, 1, &onload_async_cb, &onerror_async_cb, &onprogress_async_cb);
+#if 0
     emscripten_fetch_attr_t attr;
     emscripten_fetch_attr_init(&attr);
     strcpy(attr.requestMethod, "POST");
     attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
     attr.onsuccess = &onload_async_cb;
     attr.onerror = &onerror_async_cb;
+    //    attr.onprogress = &onprogress_async_cb;
     emscripten_fetch(&attr,buf3);
+#endif
 #else
   { // progressbar
   int s = url.size();
