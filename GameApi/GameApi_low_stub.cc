890bb6bdb0c78420905d56604c64ecaacce4a5fb
Tero Pulkkinen <terop@kotiposti.net>
2019-07-04 15:34:51 +0300

    ix##5

--- GameApi/GameApi_low_stub.cc
+++ GameApi/GameApi_low_stub.cc
@@ -142,7 +142,7 @@ public:
   virtual void glVertexPointer(int,int,int,void*) { }
   virtual void glNormalPointer(int,int,void*) { }
   virtual void glRotatef(float,float,float,float) {}
-  virtual const unsigned char *glGetString( int name ) { return ""; }
+  virtual const unsigned char *glGetString( int name ) { return 0; }
 
   //virtual void glEnableClientState(int) { }
   //virtual void glDisableClientState(int) { }
