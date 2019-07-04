45b89e6edc89ed4bb3ddcac402eda8bfbe0d5187
Tero Pulkkinen <terop@kotiposti.net>
2019-07-04 15:04:40 +0300

    debug prints.

--- GameApi/Main.cc
+++ GameApi/Main.cc
@@ -464,9 +464,9 @@ Low_SDL_Surface *InitSDL2(int scr_x, int scr_y, bool vblank, bool antialias, boo
     }
 
 #endif
-  //std::cout << "Vendor: " << glGetString(GL_VENDOR)<< std::endl;
-  //std::cout << "Renderer:" << glGetString(GL_RENDERER)<< std::endl;
-  //std::cout << "Version:" << glGetString(GL_VERSION) << std::endl;
+  std::cout << "Vendor: " << glGetString(GL_VENDOR)<< std::endl;
+  std::cout << "Renderer:" << glGetString(GL_RENDERER)<< std::endl;
+  std::cout << "Version:" << glGetString(GL_VERSION) << std::endl;
   
 
   vblank = true;
