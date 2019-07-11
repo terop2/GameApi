80afcd11d719b30ade2b144f2a9848543801ce3d
Tero Pulkkinen <terop@kotiposti.net>
2019-07-11 05:18:50 +0300

    shader printing removed.

--- GameApi/emmake.sh
+++ GameApi/emmake.sh
@@ -2,5 +2,5 @@
 /root/emscripten3/emsdk/emsdk_env.sh
 source set_paths_emlinux.sh
 make -j 4 -f Makefile.LinuxEm
-(cd web_page;make -f Makefile.LinuxEm pthreads_fixed)
+(cd web_page;make -f Makefile.LinuxEm pthreads_both)
 
