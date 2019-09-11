d7eb196ca4446b46f57688186e02f434ce0f6d06
Tero Pulkkinen <terop@kotiposti.net>
2019-09-11 11:05:52 +0300

    now firefox+no-pthreads combination works.

--- GameApi/emmake.sh
+++ GameApi/emmake.sh
@@ -2,5 +2,6 @@
 /home/terop/emscripten/emsdk/emsdk_env.sh
 source set_paths_emlinux.sh
 make -j 4 -f Makefile.LinuxEm
+make -j 4 -f Makefile.LinuxEm nothreads
 (cd web_page;make -f Makefile.LinuxEm pthreads_both)
-
+(cd web_page;make -f Makefile.LinuxEm both)
