6061bbdbd170e1cc16752e083b5d9d99f274fd44
Tero Pulkkinen <terop@kotiposti.net>
2020-02-24 00:49:39 +0200

    scripts added.

---
+++ GameApi/editor/release/copy_files.bat
@@ -0,0 +1,6 @@
+copy ..\..\libgameapi.dll .
+strip libgameapi.dll
+copy ..\..\libgamedep.dll .
+strip libgamedep.dll
+copy ..\a.exe editor\edit.exe
+strip editor\edit.exe
---
+++ GameApi/editor/release/make_msi.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" Builder.wxs
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" Builder.wixobj
