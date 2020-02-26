5c8e5af37dfd918e8e566663d980312abb81585d
Tero Pulkkinen <terop@kotiposti.net>
2020-02-26 02:38:52 +0200

    signing implemented.

--- GameApi/editor/release/make_msi.bat
+++ GameApi/editor/release/make_msi.bat
@@ -1,2 +1,7 @@
+@echo off
+if "%1"=="" ECHO make_msi password
+if "%1"=="" GOTO END
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" Builder.wxs
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" Builder.wixobj
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /f ..\guid\Builder.pfx /p "%1" Builder.msi
+:END
