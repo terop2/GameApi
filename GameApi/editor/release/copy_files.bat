a829f49fcf4f4fd2e65740a7b72821516f29bf30
Tero Pulkkinen <terop@kotiposti.net>
2021-12-08 01:26:12 +0200

    new release

--- GameApi/editor/release/copy_files.bat
+++ GameApi/editor/release/copy_files.bat
@@ -1,3 +1,9 @@
+copy ..\atlas0.txt editor\atlas0.txt
+copy ..\atlas1.txt editor\atlas1.txt
+copy ..\atlas2.txt editor\atlas2.txt
+copy ..\atlas_bm0.ppm editor\atlas_bm0.ppm
+copy ..\atlas_bm1.ppm editor\atlas_bm1.ppm
+copy ..\atlas_bm2.ppm editor\atlas_bm2.ppm
 copy ..\..\libgameapi.dll .
 strip libgameapi.dll
 copy ..\..\libgamedep.dll .
