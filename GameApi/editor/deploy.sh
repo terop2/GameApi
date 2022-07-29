913c3382f0a56527ddc19745fb12e23174a24442
Tero Pulkkinen <terop@kotiposti.net>
2022-07-30 01:19:06 +0300

    overwrite zip

--- GameApi/editor/deploy.sh
+++ GameApi/editor/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $1 ]]; then
-    (cd ~/.gameapi_builder/deploy/engine; unzip $1)
+    (cd ~/.gameapi_builder/deploy/engine; unzip -o $1)
     (cd ~/.gameapi_builder/deploy/engine; cat ./gameapi.js |sed s@web_page@engine/web_page@ >gameapi2.js)
     (cd ~/.gameapi_builder/deploy/engine; mv gameapi2.js gameapi.js)
     cat ~/.gameapi_builder/gameapi_1.html ~/.gameapi_builder/gameapi_script.html ~/.gameapi_builder/gameapi_2.html ~/.gameapi_builder/gameapi_date.html ~/.gameapi_builder/gameapi_3.html >~/.gameapi_builder/deploy/gameapi_index.php
