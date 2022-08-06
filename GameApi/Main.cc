90e41dbc61f68ab067e3f11af4f366b863c22936
Tero Pulkkinen <terop@kotiposti.net>
2022-08-06 04:41:16 +0300

    remove bug.

--- GameApi/Main.cc
+++ GameApi/Main.cc
@@ -641,10 +641,12 @@ Low_SDL_Surface *InitSDL2(int scr_x, int scr_y, bool vblank, bool antialias, boo
       //SDL_GL_SetSwapInterval(int interval);
     }
 #endif
+#ifdef WINDOWS
    ogl->glDisable(Low_GL_DEPTH_TEST);
-  ogl->glDepthMask(Low_GL_FALSE);
+   ogl->glDepthMask(Low_GL_FALSE);
   ogl->glDepthFunc(Low_GL_LEQUAL);
   ogl->glEnable(Low_GL_BLEND);
+#endif
   
   ogl->glClearColor( 0, 0, 0, 0 );
   ogl->glViewport(0,0,screenx, screeny);
