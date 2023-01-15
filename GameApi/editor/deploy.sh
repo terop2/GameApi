1c8fd904300decbf625fc2a82cb677ae0017e8fe
Tero Pulkkinen <terop@kotiposti.net>
2023-01-15 16:17:20 +0200

    deploy fixed

--- GameApi/editor/deploy.sh
+++ GameApi/editor/deploy.sh
@@ -12,7 +12,7 @@ if [[ $1 ]]; then
     if [ $? -ne 0 ]; then
 	echo "mv FAIL"
     fi
-    cat $HOME/.gameapi_builder/gameapi_0.html $HOME/.gameapi_builder/gameapi_homepage.html $HOME/.gameapi_builder/gameapi_1.html $HOME/.gameapi_builder/gameapi_script.html $HOME/.gameapi_builder/gameapi_2.html $HOME/.gameapi_builder/gameapi_date.html $HOME/.gameapi_builder/gameapi_3.html >$HOME/.gameapi_builder/deploy/gameapi_index.php
+    cat $HOME/.gameapi_builder/gameapi_0.html $HOME/.gameapi_builder/gameapi_homepage.html $HOME/.gameapi_builder/gameapi_1.html $HOME/.gameapi_builder/gameapi_script.html $HOME/.gameapi_builder/gameapi_2.html $HOME/.gameapi_builder/gameapi_date.html $HOME/.gameapi_builder/gameapi_3.html >$HOME/.gameapi_builder/deploy/index.html
     if [ $? -ne 0 ]; then
 	echo "cat2 FAIL"
     fi
