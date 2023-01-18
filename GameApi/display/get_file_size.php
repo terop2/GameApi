91987b11b443410665392b4fa137c21cef052f8c
Tero Pulkkinen <terop@kotiposti.net>
2023-01-18 20:04:54 +0200

    fix

--- GameApi/display/get_file_size.php
+++ GameApi/display/get_file_size.php
@@ -5,18 +5,21 @@ $url = $_GET["url"];
 $parse = parse_url($url);
 $host = $parse['host'];
 $mode = false;
+$chunksize = 0;
 if ($host=="meshpage.org") {
   $path = $parse['path'];
   $filename = "/home/terop/html/meshpage.org" . $path;
   $size = filesize($filename);
   $splitfilename = $filename . ".aa.br";
   $mode = file_exists($splitfilename);
+  $chunksize = filesize($splitfilename);
 } else if ($host=="tpgames.org") {
   $path = $parse['path'];
   $filename = "/home/terop/html/tpgames.org" . $path;
   $size = filesize($filename);
   $splitfilename = $filename . ".aa.br";
   $mode = file_exists($splitfilename);
+  $chunksize = filesize($splitfilename);
 } else {
   $data = get_headers($url,true);
   $size = isset($data['Content-Length'])?(int) $data['Content-Length']:0;
@@ -26,4 +29,4 @@ if ($mode==true) {
   $char = 'B';
 }
 
-echo $char . $size;
+echo $char . $size . " " . $chunksize;
