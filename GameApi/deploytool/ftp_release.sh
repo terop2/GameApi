1cf9cc142e97caed7831fbc1b56722e15dc2144a
Tero Pulkkinen <terop@kotiposti.net>
2023-03-13 02:04:06 +0200

    releasing.

---
+++ GameApi/deploytool/ftp_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+INDEX=0
+copy_it()
+{
+    MACHINE=`uname -n`
+    if [ "$MACHINE" == "terop-HP-255-G8-Notebook-PC" ]; then
+	INDEX=${INDEX}+1
+	arr[${INDEX}]=$1
+     else
+       cp $1 /home/terop/html/meshpage.org/
+       echo "copying $1 to meshpage.org"
+    fi
+}
+finish()
+{
+    MACHINE=`uname -n`
+    if [ "$MACHINE" == "terop-HP-255-G8-Notebook-PC" ]; then
+	scp ${arr[@]} terop@meshpage.org:/home/terop/html/meshpage.org/
+    fi
+}
+copy_it deploytool
