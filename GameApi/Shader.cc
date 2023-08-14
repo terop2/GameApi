57e97c0f14f0bee0370641a6c344d0eb734cc79d
Tero Pulkkinen <terop@kotiposti.net>
2023-08-14 09:37:18 +0300

    colours slightly less bright

--- GameApi/Shader.cc
+++ GameApi/Shader.cc
@@ -2715,7 +2715,7 @@ s+= " perceptualRoughness = 1.0-u_GlossiFactor;\n"
     // 0.0, 0.0, 400.0
     "  color = getPointShade(light_dir2 /*vec3(0.0,0.0,400.0)*/ /*+ex_Position/100.0*/, materialInfo, normal, view);\n"
     //" color*=baseColor.rgb;\n"
-    " color+=baseColor.rgb/2.0;\n"
+    " color+=baseColor.rgb/8.0;\n"
     //"#ifdef SPEC\n"
     //" color = diffuseColor;\n"
     //"#endif\n"
@@ -4818,7 +4818,7 @@ s+=        "mrSample2.r *= u_DiffFactor.r;\n"
     // 0.0, 0.0,400.0
     "  color = getPointShade(light_dir2 /*+ex_Position/100.0*/, materialInfo, normal, view);\n"
   //"  color*=baseColor.rgb;\n"
-  "color+=baseColor.rgb/2.0;\n"
+  "color+=baseColor.rgb/8.0;\n"
   
 "#ifdef GLTF_TEX5\n"
 "#ifdef GLTF_TEX6\n"
