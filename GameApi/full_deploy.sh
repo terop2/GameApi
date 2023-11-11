03d31b9503f4a2ade4de6ee37bf6557c47ac9fcd
Tero Pulkkinen <terop@kotiposti.net>
2023-11-11 02:40:54 +0200

    fixed

--- GameApi/full_deploy.sh
+++ GameApi/full_deploy.sh
@@ -14,7 +14,7 @@ cp editor/gameapi-builder_1.0-27.deb docker/
 sudo dpkg -i editor/gameapi-builder_1.0-27.deb
 #(cd docker;sudo docker build -t builder-test:latest .)
 (cd docker;./build_tar.sh)
-cp web_page/web_page* /home/terop/html/meshpage.org/
+scp web_page/web_page* terop@ssh.meshpage.org:/home/terop/html/meshpage.org/
 scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/html/meshpage.org/
 scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/html/tpgames.org/
 scp editor/gameapi-builder_1.0-27.deb terop@ssh.meshpage.org:/home/terop/html/meshpage.org/
