bc41b8ace6bc50d3976f4c2084fdca0024a8fdf8
Tero Pulkkinen <terop@kotiposti.net>
2024-06-01 04:47:23 +0300

    fix firefox mobile.

--- GameApi/GameApi.cc
+++ GameApi/GameApi.cc
@@ -51,6 +51,7 @@ bool is_mobile_1() { return false; }
 
 bool is_mobile(GameApi::EveryApi &ev)
 {
+  if (g_gpu_vendor=="Mozi") return false;
   return is_mobile_1() || ev.mainloop_api.get_screen_width() < 800 ||(g_gpu_vendor != "NVID" && g_gpu_vendor != "AMD" && g_gpu_vendor != "WebK");
 }
 
