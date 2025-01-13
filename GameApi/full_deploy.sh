ae9a38f78166948edd58bc5bd6c9cdc160411b70
Tero Pulkkinen <terop@kotiposti.net>
2025-01-13 19:21:47 +0200

    fixed

--- GameApi/full_deploy.sh
+++ GameApi/full_deploy.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+if [[ $1 == "" ]]; then
+    echo "Usage: ./full_deploy.sh password"
+    exit
+fi
 make clean
 make -j 8
 (cd editor;make -f Makefile.Linux)
@@ -17,12 +21,20 @@ sudo dpkg -i editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb
 (cd docker;./build_tar.sh `cat ../VERSION.TXT`)
 (cd cmdline_docker;./copy_files.sh)
 (cd cmdline_docker;./build_tar.sh)
-scp web_page/engine* terop@ssh.meshpage.org:/home/terop/meshpage.org/
-scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
-scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
-scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
-scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
-scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/
-scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
-scp ./VERSION.TXT terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
+echo "STEP#1 web_page" 
+sshpass -p $1 scp web_page/engine* terop@ssh.meshpage.org:/home/terop/meshpage.org/
+echo "STEP#2 cmdline_docker"
+sshpass -p $1 scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
+echo "STEP#3 cmdline_docker2"
+sshpass -p $1 scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
+echo "STEP#4 docker"
+sshpass -p $1 scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
+echo "STEP#5 docker2"
+sshpass -p $1 scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
+echo "STEP#6 editor"
+sshpass -p $1 scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/
+echo "STEP#7 editor2"
+sshpass -p $1 scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
+echo "STEP#8 version.txt"
+sshpass -p $1 scp ./VERSION.TXT terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
 echo "SUCCESS"
