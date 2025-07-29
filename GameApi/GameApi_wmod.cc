3f5bca5b85979403b129ed5189fdebee6160f3c7
Tero Pulkkinen <terop@kotiposti.net>
2025-07-30 00:19:56 +0300

    fixed

--- GameApi/GameApi_wmod.cc
+++ GameApi/GameApi_wmod.cc
@@ -33,7 +33,7 @@ std::string hexify(std::string s)
       res+=chrs[c2];
     }
 #endif
-  std::cout << "CONVERT:" << s << " to ";
+  //std::cout << "CONVERT:" << s << " to ";
   std::string res;
   res+="*";
   int ss = s.size();
@@ -42,7 +42,7 @@ std::string hexify(std::string s)
       if (s[i]==':') s[i]='?';
       res+=s[i];
     }
-    std::cout << res << std::endl;
+    //std::cout << res << std::endl;
     return res;
 
     //return res;
