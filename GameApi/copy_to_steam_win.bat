974d656683cacdabc30f828eaa0d5288238df482
Tero Pulkkinen <terop@kotiposti.net>
2025-11-28 20:16:00 +0200

    fixed

--- GameApi/copy_to_steam_win.bat
+++ GameApi/copy_to_steam_win.bat
@@ -1,8 +1,9 @@
-@echo off
+@echo on
 if "%1"=="" echo copy_to_steam_win.bat password
 if "%1"=="" goto END
 git pull
-full_deploy_win32 %1 28 incremental steam
+call full_deploy_win32 %1 28 incremental steam
 copy c:\users\terop\cvs\GameApi\GameApi\editor\a.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\gameapi_builder.exe
 copy c:\users\terop\cvs\GameApi\GameApi\libGameApi.dll c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\libGameApi.dll
+c:\windows\system32\xcopy.exe c:\users\terop\cvs\GameApi\GameApi\gameapi_content c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\gameapi_content /E /I /Y
 :END
