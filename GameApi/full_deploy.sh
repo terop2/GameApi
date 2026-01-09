7f65ca65fd1206343dc0ef0c75c4d1c9842e8c72
Tero Pulkkinen <terop@kotiposti.net>
2026-01-09 16:43:59 +0200

    fixed

--- GameApi/full_deploy.sh
+++ GameApi/full_deploy.sh
@@ -23,7 +23,11 @@ if [[ $2 != "noweb" && $3 != "noweb" && $4 != "noweb" ]]; then
     (cd display;./ftp_package.sh $1)
     (cd php;./ftp.sh)
 fi
-(cd deploytool;./ftp_release.sh)
+if [[ $2 == "steam" || $3 == "steam" ]]; then
+    echo "SKIP"
+else
+    (cd deploytool;./ftp_release.sh)
+fi
 (cd editor;./copy_files_to_deb.sh `cat ../VERSION.TXT`)
 (cd editor;./make_deb.sh `cat ../VERSION.TXT`)
 cp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb docker/
@@ -35,21 +39,21 @@ echo $1 |sudo -S dpkg -i editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb
 if [[ $2 == "steam" || $3 == "steam" ]]; then
     echo "SKIP"
 else
-echo "STEP#1/8 web_page" 
-scp web_page/engine* terop@ssh.meshpage.org:/home/terop/meshpage.org/
-echo "STEP#2/8 cmdline_docker"
-scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
-echo "STEP#3/8 cmdline_docker2"
-scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
-echo "STEP#4/8 docker"
-scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
-echo "STEP#5/8 docker2"
-scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
-echo "STEP#6/8 editor"
-scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/
-echo "STEP#7/8 editor2"
-scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
-echo "STEP#8/8 version.txt"
-scp ./VERSION.TXT terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
+  echo "STEP#1/8 web_page" 
+  scp web_page/engine* terop@ssh.meshpage.org:/home/terop/meshpage.org/
+  echo "STEP#2/8 cmdline_docker"
+  scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
+  echo "STEP#3/8 cmdline_docker2"
+  scp cmdline_docker/gameapi-builder-docker-cmdline.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-cmdline-`cat ./VERSION.TXT`.tar.gz
+  echo "STEP#4/8 docker"
+  scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
+  echo "STEP#5/8 docker2"
+  scp docker/gameapi-builder-docker-container.tar.gz terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/gameapi-builder-docker-container-`cat ./VERSION.TXT`.tar.gz
+  echo "STEP#6/8 editor"
+  scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/
+  echo "STEP#7/8 editor2"
+  scp editor/gameapi-builder_1.0-`cat ./VERSION.TXT`.deb terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
+  echo "STEP#8/8 version.txt"
+  scp ./VERSION.TXT terop@ssh.meshpage.org:/home/terop/meshpage.org/assets/
 fi
 echo "SUCCESS"
