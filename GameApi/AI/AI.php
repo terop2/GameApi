97470be5eaf5af7edd9da8c1a401ca9e37a37c0c
Tero Pulkkinen <terop@kotiposti.net>
2026-01-21 03:32:00 +0200

    fixed

---
+++ GameApi/AI/AI.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>AI for creating 3d scenes</title>
+</head>
+<body>
+<form action="/submit" method="get">
+<label for="prompt">Prompt:</label>
+<input type="text" id="prompt" name="prompt" required>
+<button type="submit">Submit</button>
+</form>
+</body>
+</html>
---
+++ GameApi/AI/ftp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SITE=meshpage.org
+#SITE=192.168.1.104
+
+INDEX=0
+copy_it()
+{
+    MACHINE=`uname -n`
+    if [ "$MACHINE" == "terop-HP-255-G8-Notebook-PC" ]; then
+	INDEX=${INDEX}+1
+	arr[${INDEX}]=$1
+    elif [ "$MACHINE" == "terop-pc2" ]; then
+	INDEX=${INDEX}+1
+	arr[${INDEX}]=$1
+    else
+       scp $1 terop@$SITE:/home/terop/meshpage.org/
+       echo "copying $1 to $SITE"
+    fi
+}
+finish()
+{
+    MACHINE=`uname -n`
+    if [ "$MACHINE" == "terop-HP-255-G8-Notebook-PC" ]; then
+	scp ${arr[@]} terop@$SITE:/home/terop/meshpage.org/
+    fi
+    if [ "$MACHINE" == "terop-pc2" ]; then
+	scp ${arr[@]} terop@$SITE:/home/terop/meshpage.org/
+    fi
+}
+copy_it AI.php
+finish
