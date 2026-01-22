a2435168b052daff018059e1a7a77bbf075b8fce
Tero Pulkkinen <terop@kotiposti.net>
2026-01-22 22:31:48 +0200

    fixed

--- GameApi/AI/AI.php
+++ GameApi/AI/AI.php
@@ -18,28 +18,76 @@ div#box {
 }
 #prompt {
   width: 500px;
+   border-radius: 10px;
   border-width: 3px;
   border-style: solid;
   border-color: black;
   font-size: 150%;
+}
+#promptimage {
+   border-radius: 30px;
+   border-width: 3px;
+   border-style: solid;
+}
+#label {
+  font-size: 150%;
+
+}
+#labeldiv {
+   margin: 8px;
+}
+#horiz {
+   display: flex;
+   flex-direction: row;
+}
+#buttondiv {
+  margin: 8px;
 }
 </style>
 
 </head>
 <body>
 <div id="box">
+<div id="title">
+<h1>meshpage.org/AI.php</h1>
+</div>
+<div style="height: 30px">
+</div>
 <div id="image">
-<img src="promptimage.png?t=<?= time() ?>" width="600px"/>
+<img id="promptimage" src="promptimage.png?t=<?= time() ?>" width="600px"/>
 </div>
 <div style="height: 30px">
 </div>
 <div id="promptdiv">
 <form action="/submit" method="get">
-<label for="prompt">Prompt:</label>
+<div class="horizcontainer" id="horiz">
+<div class="labeldiv" id="labeldiv">
+<label class="label" for="prompt"><b>Prompt:</b></label>
+</div>
 <input type="text" id="prompt" name="prompt" required>
-<button type="submit">Submit</button>
+<div class="buttondiv" id="buttondiv">
+<button class="submit" type="submit"><b>Submit</b></button>
+</div>
+</div>
 </form>
 </div>
 </div>
+<script>
+var counter = 0;
+function change_image(a)
+{
+   var arr = ["build_1.png", "build_2.png", "build_3.png"];
+
+   var p = document.getElementById("promptimage");
+   p.src = arr[a];
+   counter++;
+   if (counter>2) { counter=0; }
+   window.setTimeout(function() { change_image(counter); },8000);
+}
+change_image(counter);
+
+</script>
+
+
 </body>
 </html>
