bc818e246f3bb3cb205eae1a6a724aa857b172e3
Tero Pulkkinen <terop@kotiposti.net>
2026-03-20 06:46:56 +0200

    fixed

--- GameApi/php/mesh_doc.php
+++ GameApi/php/mesh_doc.php
@@ -28,4 +28,4 @@ echo "<hr><pre>";
 echo "$res3";
 echo "</pre>";
 echo "<hr>";
-echo "Warning: copying code from these files should not be done to your own 3d engine. The code has potential to break your engine structure, if you do it, because the design uses tricks that you are not able to understand, even if it looks simple.";
+echo "Warning: copying code from these files should not be done to your own 3d engine. The code has potential to break your engine structure, if you do it, because the design uses tricks that you are not able to understand, even if it looks simple. It really does something very strict constraints on surrounding code, which is enforced by the compiler, low level and the type system. Steam and opengl has marked important pieces as dangerous. And it extends to large area of the code. The problem is completely invisible also/difficult to find the real reason.";
