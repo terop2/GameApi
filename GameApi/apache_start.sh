f0354b349d62cd48cf6429bc6cf6fb5173723d88
Tero Pulkkinen <terop@kotiposti.net>
2026-05-15 09:17:37 +0300

    fixed

--- GameApi/apache_start.sh
+++ GameApi/apache_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo /usr/local/apache2/bin/apachectl restart
-sudo systemctl restart caddy
+sudo systemctl restart php8.5-fpm
+sudo systemctl restart apache2
+#sudo systemctl restart caddy
