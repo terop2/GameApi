3f0d53a5524d270cf9b2e1c3a7487f4385258add
Tero Pulkkinen <terop@kotiposti.net>
2026-05-18 12:15:59 +0300

    steam release scripts improved.

--- GameApi/copy_to_steam_win.bat
+++ GameApi/copy_to_steam_win.bat
@@ -3,14 +3,40 @@ if "%1"=="" echo copy_to_steam_win.bat password
 if "%1"=="" goto END
 git pull
 call full_deploy_win32 %1 28 incremental steam
-copy c:\users\terop\cvs\GameApi\GameApi\display\gameapi_display.zip c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\gameapi_display.zip
-copy c:\users\terop\cvs\GameApi\GameApi\editor\a.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\gameapi_builder.exe
-copy c:\users\terop\cvs\GameApi\GameApi\libGameApi.dll c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\libGameApi.dll
-c:\windows\system32\xcopy.exe c:\users\terop\cvs\GameApi\GameApi\gameapi_content c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\gameapi_content /E /I /Y
-copy c:\users\terop\cvs\GameApi\GameApi\editor\release\editor\sed.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\sed.exe
-copy c:\users\terop\cvs\GameApi\GameApi\editor\deploy.bat c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\deploy.bat
-copy c:\users\terop\cvs\GameApi\GameApi\editor\release\editor\tar.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\tar.exe
-copy c:\users\terop\cvs\GameApi\GameApi\editor\release\editor\zip.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\zip.exe
-copy c:\users\terop\cvs\GameApi\GameApi\editor\release\editor\bzip2.dll c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\bzip2.dll
-copy c:\users\terop\cvs\GameApi\GameApi\editor\release\editor\unzip.exe c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win\unzip.exe
+
+set DISPLAY_DIR=c:\users\terop\cvs\GameApi\GameApi\display
+set EDITOR_DIR=c:\users\terop\cvs\GameApi\GameApi\editor
+set ROOT_DIR=c:\users\terop\cvs\GameApi\GameApi
+
+set DEPOT_DIR=c:\users\terop\cvs\steam\sdk\tools\ContentBuilder\content\GameApiBuilder\depot_win
+
+copy %DISPLAY_DIR%\gameapi_0.html %DEPOT_DIR%\gameapi_0.html
+copy %DISPLAY_DIR%\gameapi_0_seamless.html %DEPOT_DIR%\gameapi_0_seamless.html
+copy %DISPLAY_DIR%\gameapi_1.html %DEPOT_DIR%\gameapi_1.html
+copy %DISPLAY_DIR%\gameapi_2.html %DEPOT_DIR%\gameapi_2.html
+copy %DISPLAY_DIR%\gameapi_3.html %DEPOT_DIR%\gameapi_3.html
+copy %DISPLAY_DIR%\gameapi_3_seamless.html %DEPOT_DIR%\gameapi_3_seamless.html
+
+copy %EDITOR_DIR%\atlas0.txt %DEPOT_DIR%\atlas0.txt
+copy %EDITOR_DIR%\atlas1.txt %DEPOT_DIR%\atlas1.txt
+copy %EDITOR_DIR%\atlas1_t.txt %DEPOT_DIR%\atlas1_t.txt
+copy %EDITOR_DIR%\atlas2.txt %DEPOT_DIR%\atlas2.txt
+
+copy %EDITOR_DIR%\atlas_bm0.txt %DEPOT_DIR%\atlas_bm0.txt
+copy %EDITOR_DIR%\atlas_bm1.txt %DEPOT_DIR%\atlas_bm1.txt
+copy %EDITOR_DIR%\atlas_bm1_t.txt %DEPOT_DIR%\atlas_bm1_t.txt
+copy %EDITOR_DIR%\atlas_bm2.txt %DEPOT_DIR%\atlas_bm2.txt
+
+copy %DISPLAY_DIR%\get_file_size.php %DEPOT_DIR%\get_file_size.php
+
+copy %DISPLAY_DIR%\gameapi_display.zip %DEPOT_DIR%\gameapi_display.zip
+copy %EDITOR_DIR%\a.exe %DEPOT_DIR%\gameapi_builder.exe
+copy %ROOT_DIR%\libGameApi.dll %DEPOT_DIR%\libGameApi.dll
+c:\windows\system32\xcopy.exe %DEPOT_DIR%\gameapi_content /E /I /Y
+copy %EDITOR_DIR%\release\editor\sed.exe %DEPOT_DIR%\sed.exe
+copy %EDITOR_DIR%\deploy.bat %DEPOT_DIR%\deploy.bat
+copy %EDITOR_DIR%\tar.exe %DEPOT_DIR%\tar.exe
+copy %EDITOR_DIR%\release\editor\zip.exe %DEPOT_DIR%\zip.exe
+copy %EDITOR_DIR%\release\editor\bzip2.dll %DEPOT_DIR%\bzip2.dll
+copy %EDITOR_DIR%\release\editor\unzip.exe %DEPOT_DIR%\unzip.exe
 :END
--- GameApi/steam_deploy.sh
+++ GameApi/steam_deploy.sh
@@ -9,5 +9,24 @@ cp /home/terop/cvs/GameApi/GameApi/display/gameapi_display.zip /home/terop/cvs/G
 cp /home/terop/cvs/GameApi/GameApi/editor/gameapi-builder /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/
 cp /home/terop/cvs/GameApi/GameApi/cmdline/gameapi_cmdline /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/
 cp /home/terop/cvs/GameApi/GameApi/libGameApi.so /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/libs/
+
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_0.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_0.html
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_0_seamless.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_0_seamless.html
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_1.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_1.html
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_2.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_2.html
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_3.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_3.html
+cp /home/terop/cvs/GameApi/GameApi/display/gameapi_3_seamless.html /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/gameapi_3_seamless.html
+
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas0.txt /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas0.txt
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas1.txt /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas1.txt
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas1_t.txt /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas1_t.txt
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas2.txt /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas2.txt
+
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas_bm0.ppm /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas_bm0.ppm
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas_bm1.ppm /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas_bm1.ppm
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas_bm1_t.ppm /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas_bm1_t.ppm
+cp /home/terop/cvs/GameApi/GameApi/editor/atlas_bm2.ppm /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/atlas_bm2.ppm
+cp /home/terop/cvs/GameApi/GameApi/display/get_file_size.php /home/terop/cvs/GameApi/GameApi/editor/builder/GameApiBuilder/get_file_size.php
+
 (cd /home/terop/cvs/GameApi/GameApi/editor/builder/; zip -r GameApiBuilder_rel.zip GameApiBuilder)
 (cd /home/terop/cvs/GameApi/GameApi/editor/builder/;scp GameApiBuilder_rel.zip terop@ssh.meshpage.org:/home/terop/)
