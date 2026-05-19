86f0f069185ae262d49a6bb440d283ee4384f5a6
Tero Pulkkinen <terop@kotiposti.net>
2026-05-19 03:41:10 +0300

    ubuntu version upgrade to 26.04

--- GameApi/display/meshpage.php
+++ GameApi/display/meshpage.php
@@ -1432,7 +1432,7 @@ width="120" height="120" crossorigin/>
       (install docker, nvidia-container-toolkit)<br>
       sudo docker build -t builder-test:latest .<br>
       ./run.sh script.txt homepageurl<p>
-      OR ON UBUNTU 25.04:<br>
+      OR ON UBUNTU 26.04:<br>
       LD_LIBRARY_PATH=. ./gameapi_cmdline --file script.txt --homepage homepageurl<br>
 </div>
 </div>
