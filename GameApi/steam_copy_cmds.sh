5957967dce484b06617f68bf4b96073811f62765
Tero Pulkkinen <terop@kotiposti.net>
2026-06-02 14:23:11 +0300

    new script. not yet in use.

---
+++ GameApi/steam_copy_cmds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp /usr/bin/cat editor/builder/GameApiBuilder/
+cp /usr/bin/cp editor/builder/GameApiBuilder/
+cp /usr/bin/pwd editor/builder/GameApiBuilder/
+cp /usr/bin/stdbuf editor/builder/GameApiBuilder/
+cp /usr/bin/unzip editor/builder/GameApiBuilder/
+cp /usr/bin/zip editor/builder/GameApiBuilder/
+cp /usr/bin/mv editor/builder/GameApiBuilder/
+cp /usr/bin/curl editor/builder/GameApiBuilder/
